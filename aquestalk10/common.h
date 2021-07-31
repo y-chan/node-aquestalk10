1a4ded32387662b04fc9d46d3055dddeaeeb64d9
y-chan <y-chan@y-chan.dev>
2021-07-31 18:00:48 +0900

    add common

---
+++ aquestalk10/common.h
@@ -0,0 +1,20 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+typedef struct _AQTK_VOICE_ {
+	int bas;
+	int spd;
+	int vol;
+	int pit;
+	int acc;
+	int lmd;
+	int fsc;
+} AQTK_VOICE;
+
+typedef unsigned char* (*SYNTHE)(const AQTK_VOICE* pParam, const char* koe, int* pSize);
+typedef unsigned char* (*SYNTHE_UTF16)(const AQTK_VOICE* pParam, const unsigned short* koe, int* pSize);
+
+typedef void (*FREEWAVE)(unsigned char* wav);
+typedef int (*SETKEY)(const char* key);
+
+#endif // COMMON_H
